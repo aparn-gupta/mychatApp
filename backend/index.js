1949b4d17d703ee5e4cc2a790a8b96a9b92ad223
Aoarna1-ag <aparnagupta.csa@gmail.com>
2026-04-12 21:14:17 +0530

    fetching and sending data to aws dynamo db

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,19 @@ const { Server } = require("socket.io");
 const http = require("http");
 const express = require("express");
 const cors = require("cors");
-
+const dotenv = require("dotenv");
+dotenv.config();
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const {
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+  ScanCommand,
+} = require("@aws-sdk/lib-dynamodb");
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
@@ -11,7 +22,60 @@ app.use(
   }),
 );
 
-app.get("/", (req, res) => res.json({ message: "hellooo worllldd!!" }));
+const client = new DynamoDBClient({
+  region: "ap-south-2",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+console.log(process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY);
+
+const docClient = DynamoDBDocumentClient.from(client);
+
+app.get("/", async (req, res) => {
+  // res.json({ message: "hellooo worllldd!!" });
+  const command = new GetCommand({
+    TableName: "Users",
+    Key: { id: 1 },
+  });
+  const response = await docClient.send(command);
+  console.log(response.Item);
+});
+
+app.get("/list", async (req, res) => {
+  const command = new ScanCommand({
+    TableName: "Users",
+  });
+
+  const response = await client.send(command);
+  console.log(response.Items);
+  res.status(200).json({
+    items: response.Items,
+  });
+});
+
+app.post("/user/add", async (req, res) => {
+  console.log(req.body);
+
+  const { id, username } = req.body;
+
+  const command = new PutCommand({
+    TableName: "Users",
+    Item: {
+      id: id,
+      username: username,
+    },
+  });
+  const response = await docClient.send(command);
+  if (response?.$metadata.httpStatusCode == 200) {
+    res.status(200).json({
+      message: "User created successfully!",
+    });
+  }
+  console.log(response);
+});
 
 const server = http.createServer();
 
@@ -41,3 +105,4 @@ io.on("connection", (socket) => {
 const PORT = 3000;
 
 server.listen(PORT, () => console.log(`App is listening on ${PORT}`));
+app.listen(3001, () => console.log("Express server is listening on 3001"));

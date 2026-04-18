00a90e57d272024ba2caac1cc728da9dc76ae50a
aparn-gupta <aparnagupta813@gmail.com>
2026-04-18 23:22:08 +0530

    trying to fix cors issue

--- backend/index.js
+++ backend/index.js
@@ -16,14 +16,27 @@ const {
 const app = express();
 const { randomUUID } = require("node:crypto");
 
+const allowedOrigins = ["http://localhost:8081", "http://127.0.0.1:8081"];
+
 app.use(
   cors({
-    origin: ["http://localhost:8081", "http://localhost:8081/"],
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        console.log("Blocked by CORS. Origin was:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    optionsSuccessStatus: 200, // Some legacy browsers choke on 204
+    credentials: true,
   }),
 );
+
+app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
